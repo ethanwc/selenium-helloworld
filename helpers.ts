c1d3fec461bf16e851fba331bd97d43793e1a6df
ethanwc <ethanwc@u.washington.edu>
2020-05-07 18:45:57 -0700

    hello world

--- helpers.ts
+++ helpers.ts
@@ -1,8 +1,26 @@
-import { By, until, WebDriver } from 'selenium-webdriver';
+import { By, until, WebDriver } from "selenium-webdriver";
 
-const waitUntilTime = 20000;
+const waitUntilTime = 5000;
 
-export async function querySelector(selector: string, driver: WebDriver) {
-  const el = await driver.wait(until.elementLocated(By.css(selector)), waitUntilTime);
+export async function findByCss(selector: string, driver: WebDriver) {
+  const el = await driver.wait(
+    until.elementLocated(By.css(selector)),
+    waitUntilTime
+  );
   return await driver.wait(until.elementIsVisible(el), waitUntilTime);
+}
+
+export async function findByXpath(selector: string, driver: WebDriver) {
+  const el = await driver.wait(
+    until.elementLocated(By.xpath(selector)),
+    waitUntilTime
+  );
+  return await driver.wait(until.elementIsVisible(el), waitUntilTime);
+}
+
+export async function findByXpathMany(selector: string, driver: WebDriver) {
+  return await driver.wait(
+    until.elementsLocated(By.xpath(selector)),
+    waitUntilTime
+  );
 }
--- index.test.ts
+++ index.test.ts
@@ -1,28 +1,46 @@
-import { Builder, WebDriver } from 'selenium-webdriver';
-import 'selenium-webdriver/chrome';
-import 'selenium-webdriver/firefox';
-import 'chromedriver';
-import 'geckodriver';
+import { By, Builder, WebDriver, WebElement } from "selenium-webdriver";
+import "selenium-webdriver/chrome";
+import "selenium-webdriver/firefox";
+import "chromedriver";
+import "geckodriver";
 
-import { querySelector } from './helpers';
+import { findByCss, findByXpath, findByXpathMany } from "./helpers";
 
-const rootURL = 'https://www.mozilla.org/en-US/';
+const rootURL = "https://microsoftnews.msn.com/?pcsonly=true";
+const expected_cards = 14;
 let driver: WebDriver;
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5;
 
 beforeAll(async () => {
-  driver = await new Builder().forBrowser('chrome').build();
+  driver = await new Builder().forBrowser("firefox").build();
 });
 
 afterAll(async () => driver.quit());
 
-it('initialises the context', async () => {
+it("initialises the context", async () => {
   await driver.get(rootURL);
 });
 
-it('should click on navbar button to display a drawer', async () => {
-  const anchor = await querySelector("[href='/en-US/firefox/']", driver);
-  const actual = await anchor.getText();
-  const expected = 'Firefox';
-  expect(actual).toEqual(expected);
+it("should click infopane to the right", async () => {
+  const rightSlideButton = await findByXpath("//button[@title='Next Slide']", driver);
+  //click n-1 times
+  for (let x = 0; x < expected_cards - 1; x++) await rightSlideButton.click();
+
+  //find carousel container of cards
+  const anchor = await findByXpath(
+    "//*[@class='carousel_tabPanels-DS-card1-1']",
+    driver
+  );
+
+  // const child: WebElement = await anchor.findElement(
+  //   By.xpath("//*[@id='infopane-0-ComplexContentPreview']")
+  // );
+
+  //get carousel cards
+  const children: WebElement[] = await anchor.findElements(By.xpath("//div"));
+  console.log(children.length);
+
+  const actual = "";
+  const expected = "";
+  // expect(actual).toEqual(expected);
 });
